b01d99137da4a1fc99eb624558e5c358331de45e
juanpeixoto <juanpeixotoc@gmail.com>
2026-02-04 16:07:00 -0300

    Construtores att

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,15 +2,26 @@
 //Construtores
 // construtor tem que ter o mesmo nome da classe
 
-Console.WriteLine("kkkkk");
+Console.WriteLine("##Construtores");
 
-Aluno aluno = new Aluno();
-Console.WriteLine(aluno.Nome);
-Console.WriteLine(aluno.Idade);
+Aluno aluno = new Aluno("Maria", 22, "F", "aprovada");
+
+Console.WriteLine(aluno.Nome == null ? "null" : aluno.Nome);
+Console.WriteLine(aluno.Idade == 0 ? 0 : aluno.Idade);
 Console.WriteLine(aluno.Sexo);
 Console.WriteLine(aluno.Aprovado);
+
+
 public class Aluno
 {
+    public Aluno(string nome, int idade, string sexo, string aprovado)
+    {
+        Nome = nome;
+        Idade = idade;
+        Sexo = sexo;
+        Aprovado = aprovado;
+    }
+
     public string? Nome;
     public int Idade;
     public string? Sexo;
