5ce4ffb3ecb3704759f540494dcdf63e56da0eb0
juanpeixoto <juanpeixotoc@gmail.com>
2026-02-04 16:05:22 -0300

    Construtores

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Construtores
+// construtor tem que ter o mesmo nome da classe
+
+Console.WriteLine("kkkkk");
+
+Aluno aluno = new Aluno();
+Console.WriteLine(aluno.Nome);
+Console.WriteLine(aluno.Idade);
+Console.WriteLine(aluno.Sexo);
+Console.WriteLine(aluno.Aprovado);
+public class Aluno
+{
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+    public string? Aprovado;
+}
+
