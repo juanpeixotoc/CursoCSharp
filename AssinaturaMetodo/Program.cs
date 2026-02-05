e85c88901ff97a8916b80443fdf90b580c635d5c
juanpeixoto <juanpeixotoc@gmail.com>
2026-02-05 11:56:28 -0300

    assinatura e exemplo pratico de metodos

---
+++ AssinaturaMetodo/Program.cs
@@ -0,0 +1,39 @@
+﻿//sobrecarga de metodos
+//1 - numero de parametros diferentes
+// 2 - tipos de dados diferentes
+//3- ordem dos parametros diferentes
+
+Email email = new Email();
+
+email.Enviar("juanpeixotob@gmail.com");
+email.Enviar("juanpeixotob@gmail.com", "Teste");
+email.Enviar("juanpeixotob@gmail.com", 15);
+email.Enviar(15, "juanpeixotob@gmail.com");
+
+public class Email
+{
+    public void Enviar(string endereco)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine($"Assunto padrao");
+
+    }
+    public void Enviar(string endereco, string assunto)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine($"{assunto}");
+
+    }
+
+    public void Enviar (string endereco,decimal valor)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine($"{valor}");
+    }
+
+    public void Enviar(decimal valor, string endereco)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine($"{valor}");
+    }
+}
---
+++ ExemplosPraticosMetodos/Program.cs
@@ -0,0 +1,55 @@
+﻿
+Cadastro cadastro = new();
+
+var cliente = cadastro.Regristrar();
+cadastro.ExibirDados(cliente);
+
+cadastro.Regristrar(cliente);
+cadastro.ExibirDados("Renda alterada:", cliente);
+
+Console.ReadKey();
+
+public class Cliente
+{
+    public string? Nome;
+    public int Idade;
+    public decimal Renda;
+
+    public Cliente(string? nome, int idade, decimal renda)
+    {
+        Nome = nome;
+        Idade = idade;
+        Renda = renda;
+    }
+
+    public Cliente()
+    {
+
+    }
+}
+
+public class Cadastro
+{
+    public Cliente Regristrar()
+    {
+        Cliente cliente = new("Maria",23,3000);
+        return cliente;
+    }
+
+    public Cliente Regristrar(Cliente cliente)
+    {
+        cliente.Renda = 3500;
+        return cliente;
+    }
+
+    public void ExibirDados (Cliente cliente)
+    {
+        Console.Write($"{cliente.Nome} {cliente.Idade} {cliente.Renda.ToString("c")}");
+    }
+
+    public void ExibirDados(string texto,Cliente cliente)
+    {
+        Console.Write ($"\n{texto}\t");
+        Console.Write($"{cliente.Nome} - {cliente.Renda.ToString("c")}");
+    }
+}
