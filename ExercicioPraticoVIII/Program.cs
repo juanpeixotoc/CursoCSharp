4969b0a74271b14dcbdd7f5bffe3b867155f5f2d
juanpeixoto <juanpeixotoc@gmail.com>
2026-02-10 15:23:29 -0300

    Structs

---
+++ ExercicioPraticoVIII/Program.cs
@@ -0,0 +1,48 @@
+﻿
+Cliente cliente = new Cliente("Kian", "kian@gmail.com", 3);
+
+
+Cliente.ExibirInfo(nome: cliente.Nome, email: cliente.Email, idade: cliente.Idade);
+Cliente.ExibirInfo(nome: cliente.Nome, email: cliente.Email);
+
+
+Console.ReadLine();
+
+
+struct Cliente
+{
+    private string nome;
+    public string? Nome { get; set; }
+    private string email;
+    public string? Email { get; set; }
+    private int idade;
+    public int Idade 
+    {
+        get { return idade;}
+      set
+        {
+            if (value < 18)
+                idade = 18;
+            else
+            {
+                idade = value;
+            }
+        }
+    }
+
+    public Cliente(string? nome, string? email, int idade)
+    {
+        Nome = nome;
+        Email = email;
+        Idade = idade;
+    }
+
+    public static void ExibirInfo(string? nome, string? email, int idade=0)
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Email: {email}");
+        Console.WriteLine($"Idade: {idade}");
+    }
+
+
+}
---
+++ Structs/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * tipo de dado definido pelo usuário que é composto por outros tipos de dados
+ * - semelhante a classe
+ * diferença: struct é um tipo de valor, enquanto a classe é um tipo de referência
+ * 
+ * usada se as instâncias forem pequenas e imutáveis, ou seja, não precisam ser modificadas após a criação
+ * e de curta duração, ou seja, não precisam ser mantidas por um longo período de tempo
+ */
+
+Cliente cliente = new Cliente();
+cliente.Nome = "João";
+cliente.Idade = 30;
+Console.WriteLine($"Nome: {cliente.Nome}, Idade: {cliente.Idade}");
+
+struct Cliente
+{
+    public string? Nome { get; set; }
+    public int Idade { get; set; }
+    
+    public Cliente (string? nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+} 
